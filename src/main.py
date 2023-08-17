b401b040260da3856c619ff3fba697a1e0a44bf0
sandymany <leticija.crnkovic@fer.hr>
2023-08-17 10:18:54 +0200

    changed url_for() and fixed images issues.

--- src/main.py
+++ src/main.py
@@ -2,25 +2,26 @@ from flask import Flask, render_template
 import yaml
 import subprocess
 
+app = Flask(__name__)
 
 @app.route('/home')
 def handle():
-	try:
-		data = {}
-		with open('data.yaml', 'r') as file:
-			data = yaml.safe_load(file)
-		return render_template("index.html", data=data['content'])
-	except Exception as e:
-		return(str(e))
+    try:
+        data = {}
+        with open('data.yaml', 'r') as file:
+            data = yaml.safe_load(file)
+        return render_template("index.html", data=data['content'])
+    except Exception as e:
+        return(str(e))
 
-@webhook.hook()        # Defines a handler for the 'push' event
-def on_push(data):
-  print(f"Got push with: {json.dumps(data, indent=4)}")
-	bashCommand = "cwm --rdf test.rdf --ntriples > test.nt"
-	devnull = open(os.devnull, 'wb') # Use this in Python < 3.3
-	Popen(['nohup', 'update.sh'], stdout=devnull, stderr=devnull)
-	exit(0)
+# @webhook.hook()        # Defines a handler for the 'push' event
+# def on_push(data):
+#     print(f"Got push with: {json.dumps(data, indent=4)}")
+#     bashCommand = "cwm --rdf test.rdf --ntriples > test.nt"
+#     devnull = open(os.devnull, 'wb') # Use this in Python < 3.3
+#     Popen(['nohup', 'update.sh'], stdout=devnull, stderr=devnull)
+#     exit(0)
 
 if __name__ == '__main__':
-	app.run(threaded=True, port=10000)
+    app.run(threaded=True, port=10000)
 
